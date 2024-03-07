ab0e28323cc5e3e87dac18b2ef226877bc71d667
toiaa <victoria@geddes.com.ar>
2024-03-07 19:52:47 +0100

    trying ORM connection with database

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { connectToDatabase } from "@/lib/database/mongoose";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -8,6 +8,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  connectToDatabase();
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
